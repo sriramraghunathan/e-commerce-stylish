d461494d9c5cdacaf049417c61a3ecb4dd799dfb
sriramraghunathan <sriramragunathan2002@gmail.com>
2025-07-09 11:26:15 +0530

    main

--- frontend/src/components/AdminLogin.jsx
+++ frontend/src/components/AdminLogin.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { auth } from "../firebase";
 import {
   createUserWithEmailAndPassword,
